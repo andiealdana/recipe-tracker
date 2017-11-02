a373e83705fc9392db70a595078fe06c97f94d24
Andie <Main@Andies-MBP.domain>
2017-11-01 23:09:16 -0700

    Added a toolbar and sign up button. Added routing through click action on button. Couldn't use the router-link method because it messed up the styling...

---
+++ client/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <span
+        class="home"
+        @click="navigateTo({name: 'home'})">
+        Recipe Tracker
+      </span>
+    </v-toolbar-title>
+    
+    <!-- TODO: Implement Browse Functionality -->
+    <!-- <v-toolbar-items>
+        <v-btn flat dark>
+        Browse
+        </v-btn>
+    </v-toolbar-items> -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn 
+        flat 
+        dark
+        @click="navigateTo({name: 'register'})">
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
+  
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  cursor: pointer;
+}
+</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,26 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-    <input 
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <div class="error" v-html="error" />
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            v-model="password"
+          ></v-text-field>
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            dark
+            color="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
